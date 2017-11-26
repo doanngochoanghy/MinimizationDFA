5af10920c18c23b45275dd3c686729789c156e2c
Hoang <doanngochoanghy@gmail.com>
2017-11-27 02:43:57 +0700

    input final states and print transition table

--- MinimizationDFA/Program.cs
+++ MinimizationDFA/Program.cs
@@ -8,30 +8,53 @@ namespace MinimizationDFA
 {
     class Program
     {
-        static void InputDFA(string filename,out int[,] table)
+        static void InputDFA(string filename,out int[,] transition_table,out int[] final_states)
         {
             string[] lines = System.IO.File.ReadAllLines(@filename);
             string[] line = lines[0].Split(' ');
             int m = int.Parse(line[0]);
             int n = int.Parse(line[1]);
-            table = new int[m, n];
+            transition_table = new int[m, n];
             for (int i = 0; i < m; i++)
             {
                 line = lines[i + 1].Split(' ');
                 for (int j = 0; j < n; j++)
                 {
-                    table[i, j] = int.Parse(line[j]);
+                    transition_table[i, j] = int.Parse(line[j]);
                 }
+            }
+            line = lines[m+1].Split(' ');
+            final_states = new int[line.Length];
+            for (int i = 0; i < line.Length; i++)
+            {
+                final_states[i] = int.Parse(line[i]);
+            }
+        }
+        static void PrintnDFA(int[,] transitionTable,int[] finalStates)
+        {
+            for (int i = 0; i < transitionTable.GetLength(0); i++)
+            {
+                Console.Write("{0} | ", i);
+                for (int j = 0; j < transitionTable.GetLength(1); j++)
+                {
+                    Console.Write(transitionTable[i,j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------");
+            for (int i = 0; i < finalStates.Length; i++)
+            {
+                Console.Write(finalStates[i]);
+                Console.Write(" ");
             }
         }
         static void Main(string[] args)
         {
             int[,] transitionTable;
-            InputDFA("input.txt",out transitionTable);
-            foreach (var item in transitionTable)
-            {
-                Console.WriteLine(item);
-            }
+            int[] finalStates;
+            InputDFA("input.txt",out transitionTable,out finalStates);
+            PrintnDFA(transitionTable, finalStates);
             Console.ReadKey();
         }
     }
