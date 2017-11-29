01221712b12d1d0eb828483d76b30cf970e50148
Hoang <doanngochoanghy@gmail.com>
2017-11-29 08:17:25 +0700

    .

--- MinimizationDFA/Program.cs
+++ MinimizationDFA/Program.cs
@@ -6,56 +6,100 @@ using System.Threading.Tasks;
 
 namespace MinimizationDFA
 {
+    class State
+    {
+        int index;
+        int next0_state;
+        int next1_state;
+        bool is_final=false;
+        public State(int i,int s0,int s1)
+        {
+            index = i;
+            next0_state = s0;
+            next1_state = s1;
+        }
+        public int Index
+        {
+            get {
+                return index;
+            }
+
+            set {
+                index = value;
+            }
+        }
+
+        public int Next0_state
+        {
+            get {
+                return next0_state;
+            }
+
+            set {
+                next0_state = value;
+            }
+        }
+
+        public int Next1_state
+        {
+            get {
+                return next1_state;
+            }
+
+            set {
+                next1_state = value;
+            }
+        }
+
+        public bool Is_final
+        {
+            get {
+                return is_final;
+            }
+
+            set {
+                is_final = value;
+            }
+        }
+    }
     class Program
     {
-        static void InputDFA(string filename,out int[,] transition_table,out int[] final_states)
+        static void InputDFA(string filename,out List<State> dfa)
         {
+            dfa = new List<State>();
             string[] lines = System.IO.File.ReadAllLines(@filename);
             string[] line = lines[0].Split(' ');
             int m = int.Parse(line[0]);
-            int n = int.Parse(line[1]);
-            transition_table = new int[m, n];
             for (int i = 0; i < m; i++)
             {
                 line = lines[i + 1].Split(' ');
-                for (int j = 0; j < n; j++)
-                {
-                    transition_table[i, j] = int.Parse(line[j]);
-                }
+                State s = new State(i,int.Parse(line[0]),int.Parse(line[1]));
+                dfa.Add(s);
             }
             line = lines[m+1].Split(' ');
-            final_states = new int[line.Length];
             for (int i = 0; i < line.Length; i++)
             {
-                final_states[i] = int.Parse(line[i]);
+                dfa[int.Parse(line[i])].Is_final = true;
             }
         }
-        static void PrintnDFA(int[,] transitionTable,int[] finalStates)
+        static void PrintnDFA(List<State> dfa)
         {
-            for (int i = 0; i < transitionTable.GetLength(0); i++)
+            foreach (var item in dfa)
             {
-                Console.Write("{0} | ", i);
-                for (int j = 0; j < transitionTable.GetLength(1); j++)
-                {
-                    Console.Write(transitionTable[i,j]);
-                    Console.Write(" ");
-                }
-                Console.WriteLine();
+                Console.WriteLine(item.Index.ToString() + "|" + item.Next0_state.ToString() + " " + item.Next1_state.ToString());
             }
-            Console.WriteLine("-------");
-            for (int i = 0; i < finalStates.Length; i++)
-            {
-                Console.Write(finalStates[i]);
-                Console.Write(" ");
-            }
+        }
+        static void CreateMarkTable(out int[,] markTable)
+        {
+
         }
         static void Main(string[] args)
         {
-            int[,] transitionTable;
-            int[] finalStates;
-            InputDFA("input.txt",out transitionTable,out finalStates);
-            PrintnDFA(transitionTable, finalStates);
+            List<State> DFA = new List<State>();
+            InputDFA("input.txt",out DFA);
+            PrintnDFA(DFA);
             Console.ReadKey();
+            
         }
     }
 }
